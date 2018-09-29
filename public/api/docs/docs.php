9cd8bfaf45be54f1d0b94f7250c1772074de6b3b
Sean Sangster <s@verity-bond.co.uk>
2018-09-29 21:14:33 +0100

    First commit. ADDED: Konnex User API specification in OpenAPI 3.0.0 format.

---
+++ public/api/docs/docs.php
@@ -0,0 +1,8 @@
+<?php
+echo '{
+        "message": "This is the Konnex Engine Public API Documentation home.",
+        "status": {
+            "code": "200",
+            "message": "OK"
+        }
+    }';
