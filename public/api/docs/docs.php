3aec8f42f5f535a7460dbdce50a1ec24e8cad2f3
Sean Sangster <s@verity-bond.co.uk>
2018-11-16 13:17:29 +0000

    Routine Maintenance

--- public/api/docs/docs.php
+++
@@ -1,8 +0,0 @@
-<?php
-echo '{
-        "message": "This is the Konnex Engine Public API Documentation home.",
-        "status": {
-            "code": "200",
-            "message": "OK"
-        }
-    }';
